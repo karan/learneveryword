651c5fc2bbda38f26776c493bc7af0d6952c28c9
Karan Goel <karan@goel.io>
2016-01-31 17:22:36 -0800

    final thing done

--- bot/bot.py
+++ bot/bot.py
@@ -1,48 +1,41 @@
+import json
+import logging
 import os
+
+import config
 
 import boto
 import tweepy
 
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, token_secret)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+auth = tweepy.OAuthHandler(config.twitter['key'], config.twitter['secret'])
+auth.set_access_token(config.twitter['token'], config.twitter['token_secret'])
 twitter = tweepy.API(auth)
 
-
-def _get_current_index():
-    if not(os.path.isfile(index_file_name)):
-        return 0
-    with open(index_file_name) as index_fh:
-        return int(index_fh.read().strip())
+s3conn = boto.connect_s3(config.aws['key'], config.aws['secret'])
+bucket = s3conn.get_bucket(config.aws['bucket'])
+data_key = bucket.get_key(config.aws['data'], validate=False)
 
 
-def _increment_index(index):
-    with open(index_file_name, "w") as index_fh:
-        index_fh.truncate()
-        index_fh.write("%d" % (index + 1))
-        index_fh.close()
-
-
-def _get_current_line(index):
-    with open(source_file_name) as source_fh:
-        # read the desired line
-        for i, status_str in enumerate(source_fh):
-            if i == index:
-                break
-        return status_str.strip()
+def get_one_word_meaning():
+    d = json.loads(data_key.get_contents_as_string())
+    ret_v = d.popitem()
+    data_key.set_contents_from_string(json.dumps(d))
+    logger.info('got thing: {}'.format(ret_v))
+    return ret_v
 
 
 def post_next():
-    index = _get_current_index()
-    status_str = _get_current_line(index)
-    if prefix:
-        status_str = prefix + status_str
-    if suffix:
-        status_str = status_str + suffix
-    twitter.update_status(status=status_str,
-                               lat=lat, long=long,
-                               place_id=place_id)
-    _increment_index(index)
+    try:
+        word, meaning = get_one_word_meaning()
+        status_str = '%s: %s' % (word, meaning)
+        twitter.update_status(status=status_str)
+        logger.info('updated status')
+    except Exception, e:
+        logger.error(e)
 
 
 def handler_lew(event, context):
